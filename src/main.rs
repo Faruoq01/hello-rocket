29398d781c012389b7b2062c6e498794c9495391
Faruk <Farukumar41@gmail.com>
2024-06-01 10:40:52 +0100

    starter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/name")]
+fn world() -> &'static str {
+    "Hello, Faruk!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/hello", routes![world])
+    .mount("/hi", routes![world])
 }
